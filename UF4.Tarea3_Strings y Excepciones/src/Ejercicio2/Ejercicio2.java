dff69853910b237ed7af68b2fea48fa36bdc8143
Alberto.Gurpegui <alberto.gurpeguiramon@gmail.com>
2018-02-28 10:49:22 +0100

    Alberto Gurpegui

--- UF4.Tarea3_Strings y Excepciones/src/Ejercicio2/Ejercicio2.java
+++ UF4.Tarea3_Strings y Excepciones/src/Ejercicio2/Ejercicio2.java
@@ -1,5 +1,44 @@
 package Ejercicio2;
 
-public class Ejercicio2 {
+import java.util.Scanner;
 
+public class Ejercicio2 {
+	 
+	String texto="\n";
+	String txt="";
+ 
+	public static void main(String args[]) {
+		new Ejercicio2();
+	}
+ 
+    public Ejercicio2() {
+    	leerArchivo();
+    	guardarArchivo();
+    }
+ 
+    private void leerArchivo()
+   {
+         Scanner sc = new Scanner(System.in);
+		   while (( texto = sc.nextLine() ) != null) 
+		      System.out.println(texto);
+ 
+		   texto = sc.toString();
+		   System.out.println(texto);
+ 
+		   texto = texto.toUpperCase();
+		   System.out.println("\n" +texto);
+   }
+ 
+   private void guardarArchivo()
+   {
+	   	try { 
+		    Scanner sc = new Scanner(texto);
+		    System.out.println(txt);
+		    while ((txt = sc.nextLine()) != null) 
+			System.out.println(txt);
+		} catch (Exception e) { 
+		    System.out.println("Final de Stream");
+		}
+   }
+ 
 }
